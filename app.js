f022ffae7e36f57297e09dc204b081af8096aaa2
Saudamini <saudamini@geekyants.com>
2024-08-11 00:37:24 +0530

    added

---
+++ app.js
@@ -0,0 +1,3 @@
+ const heading = React.createElement("h1", { "id" : "heading", "xxx" : "xyz" }, "Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
